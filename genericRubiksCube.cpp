0eca91c97d08f9a5cd77774d44d7afe41aaaa081
Jagan <jaganpokala16@gmail.com>
2025-01-30 22:16:07 +0530

    Added genericRubiksCube files

---
+++ genericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jagan on 30-01-2025.
+//
+
+#include "genericRubiksCube.h"
---
+++ genericRubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by jagan on 30-01-2025.
+//
+
+#ifndef GENERICRUBIKSCUBE_H
+#define GENERICRUBIKSCUBE_H
+
+
+
+class genericRubiksCube {
+
+};
+
+
+
+#endif //GENERICRUBIKSCUBE_H
